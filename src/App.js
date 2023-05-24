386f829411c07be44446605bffa1dc0a6300a794
Sina <bekarsina@gmail.com>
2023-05-25 01:11:35 +0330

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-
 export default function App() {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const key = "26c4d8ad14b57209671494df9bd9fcb9";
 
- const [data, setData] = useState({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=",
-    key: "26c4d8ad14b57209671494df9bd9fcb9",
-    countryValue: "",
+  const [countryValue, setCountryValue] = useState("");
+
+  const [data, setData] = useState({
     dataWeather: {
       name: "Northampton, GB",
       data: "11/16/2015",
@@ -17,71 +17,62 @@ export default function App() {
     },
   });
 
-  async function  fetchData() {
-    await fetch(
-      `${this.state.url}${this.state.countryValue}&&appid=${this.state.key}`
-    )
+  async function fetchData() {
+    await fetch(`${url}${countryValue}&&appid=${key}`)
       .then((res) => res.json())
       .then((data) => {
-        this.setState({
+        setData({
           dataWeather: {
             name: data.name,
             data: new Date().toLocaleDateString(),
             temp: Math.floor(data.main.temp - 273),
             weather: data.weather[0].main,
-            wind: data.wind.speed
+            wind: data.wind.speed,
           },
         });
       });
   }
 
   function getCityWeather(event) {
-    this.setState({
-      countryValue: event.target.value,
-    });
+    setCountryValue(event.target.value);
   }
 
   function getCurrentweather(event) {
     if (event.keyCode === 13) {
-      this.fetchData();
-
-      const today = new Date();
-
-      console.log(today)
+        fetchData();
+    } else {
+      return false;
     }
   }
 
-
-    window.addEventListener("keydown", (event) =>
-      this.getCurrentweather(event)
-    );
-    return (
-      <>
-        <div className="app-wrap">
-          <header>
-            <input
-              type="text"
-              autocomplete="off"
-              className="search-box"
-              placeholder="Search for a city..."
-              onChange={(event) => this.getCityWeather(event)}
-            />
-          </header>
-          <main>
-            <section className="location">
-              <div className="city">{this.state.dataWeather.name}</div>
-              <div className="date">{this.state.dataWeather.data}</div>
-            </section>
-            <div className="current">
-              <div className="temp">
-                {this.state.dataWeather.temp}
-                <span>°c</span>
-              </div>
-              <div className="weather">{this.state.dataWeather.weather}</div>
-              <div className="wind">{this.state.dataWeather.wind} km/h</div>
+  window.addEventListener("keydown", (event) => getCurrentweather(event));
+  return (
+    <>
+      <div className="app-wrap">
+        <header>
+          <input
+            type="text"
+            autocomplete="off"
+            className="search-box"
+            placeholder="Search for a city..."
+            onChange={(event) => getCityWeather(event)}
+          />
+        </header>
+        <main>
+          <section className="location">
+            <div className="city">{data.dataWeather.name}</div>
+            <div className="date">{data.dataWeather.data}</div>
+          </section>
+          <div className="current">
+            <div className="temp">
+              {data.dataWeather.temp}
+              <span>°c</span>
             </div>
-          </main>
-        </div>
-      </>
-    );
+            <div className="weather">{data.dataWeather.weather}</div>
+            <div className="wind">{data.dataWeather.wind} km/h</div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
